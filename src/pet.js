2803e5fa065c680f9753ce53adf0a382b15fd87b
HJ Kang <hkang07@gmail.com>
2022-10-11 10:14:57 +0100

    checkUp method refractored

--- src/pet.js
+++ src/pet.js
@@ -49,13 +49,13 @@ Pet.prototype.checkUp = function() {
     if(!this.isAlive) {
         return 'Your pet is no longer alive :(';
     }
-    if (this.fitness <= 3) {
-        if (this.hunger >= 5) {
-            return 'I am hungry AND I need a walk';
-        }
-        return 'I need a walk';
+
+    if (this.fitness <= 3 && this.hunger >= 5) {
+        return 'I am hungry AND I need a walk';
     } else if (this.hunger >= 5) {
         return 'I am hungry';
+    } else if (this.fitness <= 3) {
+        return 'I need a walk';
     } else {
         return 'I feel great!';
     };
